c054ea7034944ec532138a1c80c29ce2f59b4c87
poojakgupta <poojakgupta4@gmail.com>
2020-02-01 16:51:08 +0530

    added user for reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,6 +4,7 @@ class RepliesController < ApplicationController
 		@reply = Reply.new
 		@reply.body = params[:reply][:body]
 		@reply.comment_id = params[:comment_id]
+		@reply.user_id = current_user.id
 		@reply.save
 		@blog  = Comment.find(params[:comment_id]).blog
 		redirect_to @blog
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_083341) do
+ActiveRecord::Schema.define(version: 2020_02_01_111804) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_02_01_083341) do
     t.integer "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["comment_id"], name: "index_replies_on_comment_id"
   end
 
